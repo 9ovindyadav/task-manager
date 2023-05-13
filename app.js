4bb3add290222b33c2a399e2307ea7da6348300d
Govind <govindsvyadav@gmail.com>
2023-05-13 17:07:31 +0530

    doc- readme

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Job");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("./public"))
+
+app.get("/",(req,res)=>{
+       res.send("Server is live");    
+})
+
+app.listen(5000,()=>{
+    console.log(`Server is listening on port : 5000`);
+})
